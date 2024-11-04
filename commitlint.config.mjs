83675be3113dc0506ee37ed0a668037125d4698d
Simone Ceredi <57034077+sceredi@users.noreply.github.com>
2024-11-04 15:44:15 +0100

    ci: update commitlint.config.mjs

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,6 +1,7 @@
 export default {
   extends: ["@commitlint/config-conventional"],
   rules: {
-    "body-max-line-length": [0, "always", "Infinity"],
+    'body-max-line-length': [0, 'always', 'Infinity'],
+    'footer-max-line-length': [0, 'always', 'Infinity']
   },
 };
